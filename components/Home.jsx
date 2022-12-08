8b4633ee8f3db108641f0d845b90258bae514479
docie-ng <eudocia.ng@gmail.com>
2022-12-08 18:12:49 +1300

    add image files to webpack config and add image to homepage to test

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import KitchenStaff from '../server/public/images/kitchenstaff.png'
 
 function Home () {
-return <h1 className='heading'>Welcome to Jamila's Kitchen</h1>
-
+return (
+<>
+  <h1 className='heading'>Welcome to Jamila's Kitchen </h1>
+  <div className='home'>
+    <img src={KitchenStaff} alt='photo of kitchen staff' />
+  </div>
+</>
+)
 }
 
 export default Home
