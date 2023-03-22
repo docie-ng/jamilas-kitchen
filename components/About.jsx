e136e5697dccaba94a958fe7be77d804955d09d8
docie-ng <eudocia.ng@gmail.com>
2023-03-22 15:12:11 +1300

    write text for About Us section

--- components/About.jsx
+++ components/About.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 
 function About () {
-return <h1>About page</h1>
+return (
+<>
+<h2>
+Jamila's Kitchen is a Lebanese restaurant that specializes in hummus, tea, pita, and dolmas. 
+The restaurant was founded by three cousins who wanted to honor their beloved grandmother, Jamila, who was known for her exceptional cooking skills and her ability to bring people together.
 
+Growing up, the cousins spent many afternoons in Jamila's kitchen, watching her prepare delicious meals using only the freshest ingredients and traditional cooking methods. 
+They were always in awe of her talent and her passion for bringing people together through food. 
+It was during these moments that the cousins realized that they wanted to carry on Jamila's legacy by sharing her recipes and love of food with the world.
+
+After many years of planning, the cousins decided to open Jamila's Kitchen, a restaurant that would serve authentic Lebanese dishes made with love and care. 
+They wanted to create a space where people could come together any time of the day, whether it be for a quick lunch or a long, leisurely dinner. 
+The restaurant's warm and welcoming atmosphere, combined with the delicious food and traditional Lebanese hospitality, has made it a beloved destination for locals and tourists alike.
+
+At Jamila's Kitchen, the menu is centered around the family's favorite dishes, including the famous hummus, which is made fresh daily using a secret family recipe. 
+The menu also features a variety of teas, including traditional Lebanese mint tea, and a selection of homemade dolmas and pita bread. 
+Each dish is made with the same care and attention to detail that Jamila used in her own kitchen, and the result is a truly authentic Lebanese dining experience.
+For the cousins, Jamila's Kitchen is more than just a restaurant it's a tribute to their beloved grandmother and a celebration of her legacy. 
+They take great pride in sharing her recipes and her love of food with their customers, and they are grateful for the opportunity to bring people together through the power of food.
+
+</h2>
+</>
+)
 }
 
 export default About
