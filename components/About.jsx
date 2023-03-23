eb932a5620b3ad52eb4ab4996d900d8d04b06c6e
docie-ng <eudocia.ng@gmail.com>
2023-03-24 10:36:31 +1300

    add photo style about some more

--- components/About.jsx
+++ components/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Cooking from '../server/public/images/womencooking.jpeg'
 
 function About () {
 return (
 <>
 <div class='row'>
-  <div class='column'>hello</div>
+  <div class='column'>hihihi</div>
   <div class='column-right'>
     <h3>
       Jamila's Kitchen is a Lebanese restaurant that specializes in hummus, tea, pita, and dolmas. 
@@ -22,12 +22,16 @@ return (
       The restaurant's warm and welcoming atmosphere, combined with the delicious food and traditional Lebanese hospitality, has made it a beloved destination for locals and tourists alike.
       <br></br>
       <br></br>
+      <img className='homeImage' src={Cooking} alt='women cooking'></img>
+      <br></br>
+      <br></br>
       At Jamila's Kitchen, the menu is centered around the family's favorite dishes, including the famous hummus, which is made fresh daily using a secret family recipe. 
       The menu also features a variety of teas, including traditional Lebanese mint tea, and a selection of homemade dolmas and pita bread. 
       Each dish is made with the same care and attention to detail that Jamila used in her own kitchen, and the result is a truly authentic Lebanese dining experience.
       For the cousins, Jamila's Kitchen is more than just a restaurant it's a tribute to their beloved grandmother and a celebration of her legacy. 
       They take great pride in sharing her recipes and her love of food with their customers, and they are grateful for the opportunity to bring people together through the power of food.
     </h3>
+    
 </div>
 
 </div>
