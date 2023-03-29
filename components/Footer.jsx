e4ec022e8d2cd16ce27cb18d7b4b3740f3b2b0ce
docie-ng <eudocia.ng@gmail.com>
2023-03-29 14:41:53 +1300

    add text to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,11 @@ import React from 'react'
 function Footer () {
 return (
 <footer>
-  <p>Footer goes here</p>
+  <p>Tues - Sun</p>
+  <p>2pm - late</p>
+  <p>123 Field Street</p>
+  <p>Kingsland, Auckland</p>
+  <p>Book a table!</p>
 </footer>
 )
 
